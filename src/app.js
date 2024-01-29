70f0dd85c2272f5f3e77319a0cbfe2538212b80e
Sophie Osiecki <sophie.osiecki@icloud.com>
2024-01-29 21:53:55 +0000

    Removed hardcoded values from the html, and wrote the JS for the project, with the exception of displaying the days of the week, and updated the CSS slightly

---
+++ src/app.js
@@ -0,0 +1,73 @@
+function updateForecast(response) {
+  let forecastDay = document.querySelector("#weather-forecast");
+  forecastDay.innerHTML = "";
+  for (let i = 0; i < 4; i++) {
+    forecastDay.innerHTML += `<div class="day">
+    <img src="${response.data.daily[i].condition.icon_url}" />
+
+    <div class="forecast-max"><span class="forecast-max-temp">${Math.round(
+      response.data.daily[i].temperature.maximum
+    )}</span><span class="forecast-max-units">˚C</span></div>
+    <div class="forecast-min"><span class="forecast-min-temp">${Math.round(
+      response.data.daily[i].temperature.minimum
+    )}</span><span class="forecast-min-units">˚C</span></div>`;
+  }
+}
+
+function forecast(response) {
+  let forecastDay = document.querySelector("#weather-forecast");
+  let query = response.data.city;
+  let key = "044f639212f8b63toca06640b723a6aa";
+  let apiUrlForecast = `https://api.shecodes.io/weather/v1/forecast?query=${query}&key=${key}&units=metric`;
+  axios.get(apiUrlForecast).then(updateForecast);
+}
+
+function updateData(response) {
+  function displayCity(response) {
+    let city = response.data.city;
+    let country = response.data.country;
+    let cityElement = document.querySelector("#city");
+    let countryElement = document.querySelector("#country");
+
+    if (!city) {
+      alert("Check your input for typos!");
+    } else {
+      cityElement.innerHTML = city;
+      countryElement.innerHTML = country.toUpperCase();
+    }
+  }
+
+  function updateCurrentWeather(response) {
+    let currentTemperatureElement = document.querySelector("#current-temp");
+    let currentTemperature = Math.round(response.data.temperature.current);
+    currentTemperatureElement.innerHTML = currentTemperature;
+    let descriptionElement = document.querySelector("#current-description");
+    let description = response.data.condition.description;
+    descriptionElement.innerHTML = description;
+    let iconUrl = response.data.condition.icon_url;
+    let iconElement = document.querySelector("#current-weather-icon");
+    iconElement.innerHTML = `<img src ="${iconUrl}" />`;
+  }
+  displayCity(response);
+  updateCurrentWeather(response);
+  forecast(response);
+}
+
+function getWeatherData(cityValue) {
+  let apiKey = "044f639212f8b63toca06640b723a6aa";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityValue}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateData);
+}
+
+function handleSearchInput(event) {
+  event.preventDefault();
+
+  let cityElementInput = document.querySelector("#city-input");
+  let cityValue = cityElementInput.value;
+  getWeatherData(cityValue);
+}
+
+let searchInputElement = document.querySelector("#city-search");
+
+getWeatherData("LA");
+searchInputElement.addEventListener("submit", handleSearchInput);
